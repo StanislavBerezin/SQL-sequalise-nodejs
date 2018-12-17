ffc3754276c78567cb0a058b1f61c59ba17bbac7
Ubuntu <stanislav_berezin@hotmail.com>
2018-12-17 22:44:16 +1000

    finished sql

--- app.js
+++ app.js
@@ -3,11 +3,43 @@ const bodyParser = require('body-parser')
 const app = express()
 const sequelize = require('./db/connection')
 
+const Product = require('./models/product')
+const User = require('./models/user')
+
 app.use(bodyParser.json())
 
 require('./routes/routes')(app)
 
-sequelize.sync().then((result) => {
+app.use((req, res, next) => {
+    User.findById(1).then(user => {
+        req.user = user
+        next()
+    }).catch(err => {
+        console.log(err)
+    })
+})
+
+
+Product.belongsTo(User, {
+    constraints: true,
+    onDelete: "CASCADE"
+})
+User.hasMany(Product)
+
+sequelize.sync({
+    force: true
+}).then((result) => {
+    return User.findByPk(1)
+
+}).then(user => {
+    if (!user) {
+        User.create({
+            name: "Stas",
+            email: "test@hotmail.com"
+        })
+    }
+    return Promise.resolve(user);
+}).then(user => {
     app.listen(3000, () => {
         console.log("Listening on port 3000")
     })
